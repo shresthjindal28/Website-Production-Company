555d00fa0eb5c3eee116289d2e277dbd32cd78e8
shresthjindal28 <shresthjindal28@gmail.com>
2024-07-28 13:02:10 +0530

    packages added

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 text-7xl'>HELLO</h1>
+    </>
+  )
+}
+
+export default App
