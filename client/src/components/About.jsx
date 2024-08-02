8c96c766339b34b2b3a8ee77564300d73f82c61b
shresthjindal28 <shresthjindal28@gmail.com>
2024-08-02 14:46:29 +0530

    update about page ui

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import Design from "./Design";
+import Spline from '@splinetool/react-spline';
+
 import {
   FaGithub,
   FaFacebook,
@@ -22,25 +23,25 @@ function About() {
 
   return (
     <>
-      <div className="w-11/12 mx-auto xl:mx-0 xl:w-full xl:h-screen xl:pl-5 p-1 flex xl:flex-row xl:items-center xl:justify-center text-gray-400">
-        <div className="xl:flex xl:flex-col xl:items-start xl:justify-center flex-1">
-          <h1 className="xl:text-4xl font-bold xl:ml-5 xl:mb-3 ">
-            HI &#128075;{" "}
-          </h1>
-          <h1 className="xl:text-4xl font-bold xl:ml-5">
-            We{" "}
-            <span className="font-bold xl:text-4xl text-blue-400 ">{text}</span>
-            <span>
-              <Cursor />
-            </span>
-          </h1>
+      <div className="w-11/12 mx-auto xl:mx-0 xl:w-full xl:h-screen xl:pl-5 p-1 xl:absolute flex xl:flex-row xl:items-center xl:justify-center text-gray-400">
+        <div className=" flex items-center justify-center">
+          <div className="xl:flex xl:flex-col xl:items-start xl:justify-start flex-1">
+             <h1 className="xl:text-4xl font-bold xl:ml-5 xl:mb-3 ">
+              HI &#128075;{" "}
+            </h1>
+            <h1 className="xl:text-4xl font-bold xl:ml-5">
+              We{" "}
+              <span className="font-bold xl:text-4xl text-blue-400 ">{text}</span>
+              <span>
+                <Cursor />
+              </span>
+            </h1>
 
-          <div className="">
             <h2 className="xl:text-3xl font-normal xl:ml-5 xl:mt-5">
-              We are a dynamic team of skilled web developers, passionate about
+              We are a dynamic team of <br /> skilled web developers, passionate <br /> about
               creating innovative and efficient digital solutions.
             </h2>
-            <div className="flex ml-16 mt-6 text-gray-600 gap-8">
+            <div className="flex ml-6 mt-9 text-gray-600 gap-8">
               <FaGithub size={35} className="hover:text-white" />
               <FaFacebook size={35} className="hover:text-white" />
               <FaInstagram size={35} className="hover:text-white" />
@@ -51,11 +52,12 @@ function About() {
                 <FaLinkedinIn size={35} className="hover:text-white" />
               </a>
             </div>
+          </div >
+          <div className="w-full h-[100vh] absolute xl:relative flex justify-end flex-1">
+          <Spline
+        scene="https://prod.spline.design/uQU40Ro0vG0lyJd3/scene.splinecode" 
+      />
           </div>
-        </div>
-
-        <div className="hidden xl:block">
-          <Design w={600} h={600} />
         </div>
       </div>
     </>
