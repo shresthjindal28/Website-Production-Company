ca3278cd79a00076a6b286fba2fad7773845e7cf
shresthjindal28 <shresthjindal28@gmail.com>
2024-08-02 15:16:00 +0530

    added a loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./index.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    },6000)
+  }, [])
   return (
     <>
+    {
+      loading ? <div> <Loader/> </div> : <div className="App">
+    
       <Navbar />
       <Home />
       <About />
+      </div>
+    }
     </>
   );
 }
---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+function Loader() {
+  return (
+    <div>
+
+      <Spline
+        scene="https://prod.spline.design/gROB4qDbFQCAEqTn/scene.splinecode" 
+      />
+
+    </div>
+  )
+}
+
+export default Loader
